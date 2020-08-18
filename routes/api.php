083d9e1f4a109852f2f9370d0796ee24c31ce103
Sandi Suryadi <sandis.pragma@gmail.com>
2020-08-18 14:13:40 +0700

    [Sandi Suryadi] API Course Complete

---
+++ routes/api.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::get('mentors', 'MentorController@index');
+Route::get('mentors/{id}', 'MentorController@show');
+Route::post('mentors', 'MentorController@create');
+Route::put('mentors/{id}', 'MentorController@update');
+Route::delete('mentors/{id}', 'MentorController@destroy');
+
+Route::get('courses', 'CourseController@index');
+Route::post('courses', 'CourseController@create');
+Route::put('courses/{id}', 'CourseController@update');
+Route::get('courses/{id}', 'CourseController@show');
+Route::delete('courses/{id}', 'CourseController@destroy');
+
+Route::get('chapters', 'ChapterController@index');
+Route::get('chapters/{id}', 'ChapterController@show');
+Route::post('chapters', 'ChapterController@create');
+Route::put('chapters/{id}', 'ChapterController@update');
+Route::delete('chapters/{id}', 'ChapterController@destroy');
+
+Route::get('lessons', 'LessonController@index');
+Route::get('lessons/{id}', 'LessonController@show');
+Route::post('lessons', 'LessonController@create');
+Route::put('lessons/{id}', 'LessonController@update');
+Route::delete('lessons/{id}', 'LessonController@destroy');
+
+Route::post('image-courses', 'ImageCourseController@create');
+Route::delete('image-courses/{id}', 'ImageCourseController@destroy');
+
+Route::post('my-courses', 'MyCourseController@create');
+Route::get('my-courses', 'MyCourseController@index');
+
+Route::post('reviews', 'ReviewController@create');
+Route::put('reviews/{id}', 'ReviewController@update');
+Route::delete('reviews/{id}', 'ReviewController@destroy');
